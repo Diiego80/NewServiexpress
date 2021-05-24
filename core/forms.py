c87d0eff487949524c9fd37284c9de9a4e5dc6ac
Diiego80 <ortegavargas.die@gmail.com>
2021-05-23 23:30:43 -0400

    registro

--- core/forms.py
+++ core/forms.py
@@ -3,10 +3,18 @@ from django.db import models
 from .models import *
 import datetime
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
-    pass
+    
+    class Meta:
+        model = User
+        fields = ['username', "first_name", "last_name", "email","password1", "password2"]
+
+
+
+
 
 class UsuarioForm(forms.ModelForm):
 
